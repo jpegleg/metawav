a3a08be9fbf83a121513e1bd212cbd7f0cdcc8f4
Keegan Bowen <keegan.bowen@gmail.com>
2015-02-16 11:29:54 -0700

    Update metawav.sh

--- metawav.sh
+++ metawav.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-# Read the configuration files and write them to a log... for now.
+
 mkdir -p ~/wav-archive > /dev/null
 mkdir -p ~/wav-log > /dev/null
 mkdir -p ~/wav-json > /dev/null
+
+template=$1
 
 function readconf () {
 echo "$(date) Metawav - Reading your JSON files in ~/wav-archive/.log/"
@@ -12,18 +14,22 @@ for x in $(ls ~/wav-json/*.[j-J][s-S][o-O][n-N]); do
    cat "$x" >> ~/wav-log/run.log
 done
 }
-# This is the most simple way to do this part. Just use the options passed to specify the metadata to append.
 
 function append () {
 for x in $(ls ~/wav-archive/ ); do
   echo $(date +'%m%d%Y') >> ~/wav-log/run.log;
   echo "Appending $x" >> ~/wav-log/run.log;
   cd ~/wav-archive/
-  y=$(cat "$x" ~/wav-json/"$1")
+  y=$(cat "$x" ~/wav-json/"$template")
   echo "${y[@]}" > "$x".tmp &&
   mv "$x".tmp "$x"
 done
 }
+
+readconf
+
+append
+
 
 # More functions and some kind of logic sequence will happen some day.
 # For now, simple.
