ccb941d59c868d72e67591f1cb041c7536fdeb20
Keegan Bowen <keegan.bowen@gmail.com>
2015-02-15 20:39:15 -0700

    Update metawav.sh

--- metawav.sh
+++ metawav.sh
@@ -1,27 +1,34 @@
 #!/bin/bash
-# Read the configuration files and parse them to ensure they are valid JSON.
+# Read the configuration files and write them to a log... for now.
+mkdir -p ~/wav-archive > /dev/null
+mkdir -p ~/wav-log > /dev/null
+mkdir -p ~/wav-json > /dev/null
+e
 
 function readconf () {
 echo "$(date) Metawav - Reading your JSON files in ~/wav-archive/.log/"
-for x in $(ls ~/wav-archive/.log/*.[j-J][s-S][o-O][n-N]); do
-   echo "Doing validation on $x" >> ~/wav-archive/.log/run.log
-   echo $(date) >> ~/wav-archive/.log/run.log
-   cat "$x" >> ~/wav-archive/.log/run.log
+for x in $(ls ~/wav-json/*.[j-J][s-S][o-O][n-N]); do
+   echo "Doing validation on $x" >> ~/wav-log/run.log
+   echo $(date) >> ~/wav-log/run.log
+   cat "$x" >> ~/wav-log/run.log
 done
 }
 # This is the most simple way to do this part. Just use the options passed to specify the metadata to append.
 
 function append () {
 for x in $(ls ~/wav-archive/ ); do
-  echo $(date +'%m%d%Y') >> ~/wav-archive/.log/run.log;
-  echo "Appending $x" >> ~/wav-archive/.log/run.log;
-  cat ~/wav-archive/.log/"$@" >> "$x";
+  echo $(date +'%m%d%Y') >> ~/wav-log/run.log;
+  echo "Appending $x" >> ~/wav-log/run.log;
+  cd ~/wav-archive/
+  y=$(cat "$x" ~/wav-json/"$1")
+  echo "${y[@]}" > "$x".tmp &&
+  mv "$x".tmp "$x"
 done
 }
 
 # More functions and some kind of logic sequence will happen some day.
 # For now, simple.
 
-readconf
+readconf &&
 
 append
