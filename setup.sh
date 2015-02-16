e56000418ccb34847ec71ee3cf07e9dd0efa6b91
Keegan Bowen <keegan.bowen@gmail.com>
2015-02-15 22:41:56 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,7 +9,9 @@ echo "...got em." &&
 
 echo "Installing..." &&
 cp ./metawav.sh /usr/bin/metawav &&
-mkdir -p $HOME/wav-archive/.log &&
-cp template.metawav.json $HOME/wav-archive/.log &&
+mkdir -p $HOME/wav-archive/ &&
+mkdir -p $HOME/wav-json/ &&
+mkdir -p $HOME/wav-log/ &&
+cp template.metawav.json $HOME/wav-json/.log &&
 
 echo "Ready to rock." 
